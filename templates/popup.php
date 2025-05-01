13155e4241a175d52bf129fec5ff393b7a84fb82
obara501 <geofreyongeta501@gmail.com>
2025-05-01 11:50:07 +0300

    link to mega play instagram

--- templates/popup.php
+++ templates/popup.php
@@ -1,3 +1,4 @@
+<!-- Jemsa Media Tech video popup -->
 <!-- <div class="uk-modal-dialog uk-width-auto uk-margin-auto-vertical uk-background-secondary uk-light">
     <button class="uk-modal-close-outside" type="button" uk-close></button>
     <video autoplay loop muted playsinline class="uk-border-rounded">
@@ -9,20 +10,17 @@
 
 </div> -->
 
+<!-- Mega Play Arcade video popup -->
 <div class="uk-modal-dialog uk-margin-auto-vertical uk-flex uk-flex-center uk-flex-middle " style="width: 100%">
     <button class="uk-modal-close-outside" type="button" uk-close></button>
     <video autoplay loop muted playsinline uk-video class="uk-border-rounded">
         <source src="images/MEGA BANNER.mp4" type="video/mp4">
     </video>
 
-    <!-- WhatsApp click overlay -->
-    <a href="https://api.whatsapp.com/send?phone=254740953042&text=Hi%2C%20I'm%20interested!" 
+    <!-- Click overlay -->
+    <a href="https://www.instagram.com/megaplay_arcade?igsh=aHpwaHUwMG5yaGU4" 
        target="_blank" 
        class="click-overlay"
        id="click-to-whatsapp">
     </a>
 </div>
-
-
-
-
